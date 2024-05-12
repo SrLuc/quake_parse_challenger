1a1df6c82cdd9c3f9fbe1abac51a748a33ac1a6b
SrLuc <ruanlucenaramos@gmail.com>
2024-05-11 21:07:38 -0300

    chore:adding dotenv library and modify getNameById router

--- app.ts
+++ app.ts
@@ -1,12 +1,10 @@
-import { Request, Response } from "express";
-
 const express = require("express");
 const app = express();
 
 import * as gameController from "./controllers/gameController";
 
 app.get("/games", gameController.getGames);
-app.get("/games/:id", gameController.getGameById);
+app.get("/game/:id", gameController.getGameById);
 app.get("/reports", gameController.getReports);
 app.get("/ranking", gameController.getRanking);
 
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
+require("dotenv/config");
 import app from "./app";
 
-app.listen(9999, () => {
+app.listen(process.env.API_PORT, () => {
   console.log("Games processed!");
-  console.log(`server started at http://localhost:9999`);
+  console.log(`server started at http://localhost:${process.env.API_PORT}`);
 });
